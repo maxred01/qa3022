d41c4b4458e7e0fc83b8e019af4976bf8a3de21e
Scholakau-Siarhei <xoykaktus@gmail.com>
2023-06-05 10:24:32 +0300

    dz

---
+++ dz.py
@@ -0,0 +1,13 @@
+Примеры локаторов CSS:
+https://www.slayer.net/
+[alt="Slayer - Repentless | The Official Slayer Site"]
+https://www.slayer.net/discography
+div[style*=ab67616d00001e029bc8ae475d19a4dd87672034]
+https://www.slayer.net/tour
+#yui_3_17_2_1_1685683292309_282
+https://www.bandsintown.com/a/386
+# Этот правильно? Есть дубль, но что за он хз.
+[href="https://www.bandsintown.com/e/101363740-slayer-at-the-forum?came_from=251&utm_medium=web&utm_source=artist_page&utm_campaign=event"]
+class="PBpcsHwgCdz4X2sF6kQ_"
+
+img[alt='Slayer']
