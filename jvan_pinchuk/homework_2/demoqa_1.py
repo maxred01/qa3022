752a92e79df63f4039be50118faab14966a98fab
jvanpinchuk <j.v.a.n.pinchuk@gmail.com>
2023-06-10 23:03:43 +0300

    Ivan Pinchuk: Homework 2 completed

--- jvan_pinchuk/homework_2/demoqa_1.py
+++ jvan_pinchuk/homework_2/demoqa_1.py
@@ -14,7 +14,6 @@ a = btn_elements.is_displayed()
 print(a)
 check.is_true(a)
 btn_elements.click()
-
 btn_main = chrome_driver.find_element(By.CSS_SELECTOR, "img[src='/images/Toolsqa.jpg']")
 btn_main.click()
 
@@ -22,9 +21,9 @@ time.sleep(5)
 # Forms
 btn_forms = chrome_driver.find_element(By.XPATH, "(//div[@class='card mt-4 top-card'])[2]")
 
-a = btn_forms.is_displayed()
-print(a)
-check.is_true(a)
+b = btn_forms.is_displayed()
+print(b)
+check.is_true(b)
 btn_forms.click()
 btn_main = chrome_driver.find_element(By.CSS_SELECTOR, "img[src='/images/Toolsqa.jpg']")
 btn_main.click()
@@ -32,9 +31,9 @@ btn_main.click()
 
 # Alerts
 btn_alerts = chrome_driver.find_element(By.XPATH, "(//div[@class='card mt-4 top-card'])[3]")
-a = btn_alerts.is_displayed()
-print(a)
-check.is_true(a)
+c = btn_alerts.is_displayed()
+print(c)
+check.is_true(c)
 btn_alerts.click()
 btn_main = chrome_driver.find_element(By.CSS_SELECTOR, "img[src='/images/Toolsqa.jpg']")
 btn_main.click()
@@ -42,35 +41,29 @@ btn_main.click()
 
 # Widgets
 btn_widgets = chrome_driver.find_element(By.XPATH, "(//div[@class='card mt-4 top-card'])[4]")
-a = btn_widgets.is_displayed()
-print(a)
-check.is_true(a)
+d = btn_widgets.is_displayed()
+print(d)
+check.is_true(d)
 btn_widgets.click()
 btn_main = chrome_driver.find_element(By.CSS_SELECTOR, "img[src='/images/Toolsqa.jpg']")
 btn_main.click()
 
-
 # Interactions
 btn_interactions = chrome_driver.find_element(By.XPATH, "(//div[@class='card mt-4 top-card'])[5]")
-
-a = btn_interactions.is_displayed()
-print(a)
-check.is_true(a)
+e = btn_interactions.is_displayed()
+print(e)
+check.is_true(e)
 btn_interactions.click()
 btn_main = chrome_driver.find_element(By.CSS_SELECTOR, "img[src='/images/Toolsqa.jpg']")
 btn_main.click()
 
-
 # Book store
 chrome_driver.execute_script("window.scrollBy(0, 500);")
 btn_bookstore = chrome_driver.find_element(By.XPATH, "(//div[@class='card mt-4 top-card'])[6]")
-a = btn_bookstore.is_displayed()
-print(a)
-check.is_true(a)
+g = btn_bookstore.is_displayed()
+print(g)
+check.is_true(g)
 btn_bookstore.click()
 time.sleep(5)
 chrome_driver.quit()
-
-
-
 
