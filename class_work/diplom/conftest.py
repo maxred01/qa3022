a3f3072d1a85d7eaf6037bc4735f5d3c2a988ba7
MAX <55154175+maxred01@users.noreply.github.com>
2023-06-27 22:10:01 +0300

    Update conftest.py

--- class_work/diplom/conftest.py
+++ class_work/diplom/conftest.py
@@ -1,32 +1,39 @@
 import requests
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-import pytest
 import pytest, requests, logging
 
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import base64
 from selenium.webdriver import DesiredCapabilities
-# from api_test.app import Application
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.proxy import *
 
 
-# @pytest.fixture(scope='function')
-# def driver():
-#     options = Options()
-#     options.add_argument('log-level=3')
-#     options.add_experimental_option('excludeSwitches', ['enable-logging'])
-#     options.add_argument('window-size=1920,1080')
-#     my_driver = webdriver.Chrome(options=options)
-#     my_driver.implicitly_wait(10)
-#     yield my_driver
-#     my_driver.quit()
+# 1. requests - библиотека для отправки HTTP-запросов и получения ответов. Она используется для взаимодействия с веб-серверами и получения данных с веб-страниц.
+# 2. selenium - библиотека для автоматизации действий веб-браузера. Она используется для создания автоматизированных тестов, скрапинга данных с веб-страниц и других задач, связанных с веб-браузером.
+# 3. pytest - библиотека для создания и выполнения автоматических тестов. Она используется для тестирования кода, написанного на Python, и проверки его корректности.
+# 4. logging - библиотека для регистрации сообщений, создания журналов и отслеживания ошибок в приложениях. Она используется для записи сообщений об ошибках и отладочной информации в файлы журналов.
+# 5. base64 - библиотека для кодирования и декодирования данных в формате Base64. Она используется для конвертации данных в строковый формат, который может быть передан веб-браузеру.
+# 6. DesiredCapabilities - класс, предоставляемый Selenium WebDriver, для настройки параметров браузера. Он используется для установки параметров, таких как версия браузера, операционная система и другие настройки.
+# 7. ChromeDriverManager - менеджер драйверов для браузера Google Chrome. Он используется для автоматической загрузки и установки драйвера Chrome, который необходим для работы Selenium WebDriver с браузером Chrome.
+# 8. selenium.webdriver.common.proxy - модуль, предоставляемый Selenium WebDriver, для настройки прокси-сервера. Он используется для установки параметров прокси-сервера, таких как адрес и порт.
 
 
 @pytest.fixture(scope='function')
 def driver():
+    """ Фикстура driver() создает экземпляр объекта WebDriver
+        и устанавливает его настройки. Фикстура устанавливает режим
+        без графического интерфейса (--headless) и отключает использование
+        GPU (--disable-gpu). Фикстура также устанавливает низкий уровень
+        журналирования (--log-level=3) и исключает журналирование
+        (--excludeSwitches) от ChromeDriver. Затем фикстура создает
+        экземпляр объекта WebDriver с помощью ChromeDriverManager и
+        максимизирует окно браузера. Фикстура также устанавливает время
+        неявного ожидания на 10 секунд. После прохождения всех тестов
+        фикстура закрывает браузер"""
+
     options = Options()
     options.add_argument("--headless")
     options.add_argument("--disable-gpu")
@@ -42,6 +49,14 @@ def driver():
 
 @pytest.fixture(scope='function')
 def change_pass_back():
+    """ Вторая фикстура change_pass_back() выполняет обратное
+        изменение пароля. Эта фикстура используется в тестах,
+        которые изменяют пароль, чтобы вернуть исходный пароль
+        после теста. Фикстура использует библиотеку requests
+        для создания сеанса и отправки POST-запросов на сервер.
+        Фикстура отправляет запросы на сервер, чтобы вернуть
+        исходный пароль для пользователя"""
+
     yield
     session = requests.Session()
     data = {
