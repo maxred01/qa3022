62ac98327e3480bba293f9af5f2b3dbf402c9e9d
natali <test@gmail.com>
2023-07-10 21:54:16 +0300

    fixfix

--- natalya_ivanenko/hw_6/tests/test_auth.py
+++ natalya_ivanenko/hw_6/tests/test_auth.py
@@ -5,7 +5,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.action_chains import ActionChains
-from natalya_ivanenko.hw_6.pages.locators import auth_locators
+from hw_6.pages.locators import auth_locators
 
 # Инициализация драйвера Chrome
 driver = webdriver.Chrome()
